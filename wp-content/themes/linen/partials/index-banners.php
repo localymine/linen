2cbbc86d0827cd6ffa402729931ce5077b1ace91
khang.ld <khang.ld@aris-vn.com>
2017-01-09 17:35:51 +0700

    processing home page

--- wp-content/themes/linen/partials/index-banners.php
+++ wp-content/themes/linen/partials/index-banners.php
@@ -1,3 +1,29 @@
+<?php
+$i_slider = 0;
+$args = array(
+    'post_type' => 'index_banners',
+    'posts_per_page' => 1,
+    'orderby' => array('date' => 'DESC'),
+);
+$loop = new WP_Query($args);
+$home_slider = array();
+if ($loop->have_posts()) {
+    while ($loop->have_posts()) {
+        $loop->the_post();
+        while (have_rows('images')) {
+            the_row();
+            $image = get_sub_field('image');
+            $full = $image['url'];
+            $home_slider[$i_slider]['image'] = $full;
+            $home_slider[$i_slider]['url'] = get_sub_field('url');
+            $home_slider[$i_slider]['description'] = get_sub_field('description');
+            $i_slider++;
+        }
+    }
+}
+//
+wp_reset_postdata();
+?>
 <div class="index_banners">
     <div class="container">
         <div class="main_holder">
