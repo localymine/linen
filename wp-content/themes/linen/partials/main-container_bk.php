60d679280a564559912165e219873126db4ac767
khang.ld <khang.ld@aris-vn.com>
2017-01-10 17:36:48 +0700

    backup template product

---
+++ wp-content/themes/linen/partials/main-container_bk.php
@@ -0,0 +1,248 @@
+<div class="main-container col1-layout">
+    <div class="container">
+        <div class="main">
+            <div class="col-main">
+                <div class="padding-s">
+                    <?php
+                    // TODO
+                    get_template_part('partials/em/register', 'validation-message');
+                    ?>
+                    <div class="widget widget-new-products">
+                        <div class="widget-title">
+                            <h2>New Products</h2>
+                        </div>
+                        <div class="carousel_nav">
+                            <a class="btns prev_new"><span class="material-design-keyboard54"></span></a>
+                            <a class="btns next_new"><span class="material-design-keyboard53"></span></a>
+                        </div>
+
+                        <div class="owl-carousel-wrapper owl-new-products products-grid">
+                            <div class="owl-carousel owl-new owl-theme" id="owl-new-products">
+
+                                <div class="carousel_col">
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <div class="label-product">
+                                                    <span class="sale">Sale</span>
+                                                </div>
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/73D7CC" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                            <div class="product-details">
+                                                <h2 class="product-name" itemprop="name"><a href="http://ld-magento.template-help.com/magento_58878/suit-blazer.html" title="Suit blazer">Suit blazer</a></h2>
+                                                <div class="price-box">
+                                                    <span class="regular-price" id="product-price-83">
+                                                        <span class="price">$150.00</span>
+                                                    </span>
+                                                </div>
+                                                <div class="ratings">
+                                                    <div class="rating-box stars">
+                                                        <i class="review-icon"></i>
+                                                        <i class="review-icon"></i>
+                                                        <i class="review-icon"></i>
+                                                        <i class="review-icon"></i>
+                                                        <i class="review-icon"></i>
+                                                        <div class="rating">
+                                                            <div class="mask">
+                                                                <i class="review-icon"></i>
+                                                                <i class="review-icon"></i>
+                                                                <i class="review-icon"></i>
+                                                                <i class="review-icon"></i>
+                                                                <i class="review-icon"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <span class="amount"><a href="#" onclick="var t = opener ? opener.window : window; t.location.href = 'http://ld-magento.template-help.com/magento_58878/review/product/list/id/83/'; return false;">1 Review(s)</a></span>
+                                                </div>
+                                                <div class="actions">
+                                                    <a href="http://ld-magento.template-help.com/magento_58878/checkout/cart/add/uenc/aHR0cDovL2xkLW1hZ2VudG8udGVtcGxhdGUtaGVscC5jb20vbWFnZW50b181ODg3OC8,/product/83/form_key/Pu88seeoujm3gNCs/" class="btns_cart" title="Add to Cart"><span class="fl-line-icon-set-shopping63"></span></a>
+                                                    <ul class="add-to-links">
+                                                        <li><a href="http://ld-magento.template-help.com/magento_58878/wishlist/index/add/product/83/form_key/Pu88seeoujm3gNCs/" class="link-wishlist"><span>Add to Wishlist</span></a></li>
+                                                        <li><a href="http://ld-magento.template-help.com/magento_58878/catalog/product_compare/add/product/83/uenc/aHR0cDovL2xkLW1hZ2VudG8udGVtcGxhdGUtaGVscC5jb20vbWFnZW50b181ODg3OC8,/form_key/Pu88seeoujm3gNCs/" class="link-compare"><span>Add to Compare</span></a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <div class="label-product">
+                                                    <span class="new">New</span>
+                                                    <span class="sale">Sale</span>
+                                                </div>
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/F58AB7" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                            <div class="product-details">
+                                                <h2 class="product-name" itemprop="name"><a href="http://ld-magento.template-help.com/magento_58878/suit-blazer.html" title="Suit blazer">Suit blazer</a></h2>
+                                                <div class="price-box">
+                                                    <p class="special-price">
+                                                        <span class="price-label">Special Price</span>
+                                                        <span class="price" id="product-price-101">$35.00</span>
+                                                    </p>
+                                                    <p class="old-price">
+                                                        <span class="price-label">Regular Price:</span>
+                                                        <span class="price" id="old-price-101">$41.00</span>
+                                                    </p>
+                                                </div>
+                                                <div class="ratings">
+                                                    <div class="rating-box stars">
+                                                        <i class="review-icon"></i>
+                                                        <i class="review-icon"></i>
+                                                        <i class="review-icon"></i>
+                                                        <i class="review-icon"></i>
+                                                        <i class="review-icon"></i>
+                                                        <div class="rating">
+                                                            <div class="mask">
+                                                                <i class="review-icon"></i>
+                                                                <i class="review-icon"></i>
+                                                                <i class="review-icon"></i>
+                                                                <i class="review-icon"></i>
+                                                                <i class="review-icon"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <span class="amount"><a href="#" onclick="var t = opener ? opener.window : window; t.location.href = 'http://ld-magento.template-help.com/magento_58878/review/product/list/id/83/'; return false;">1 Review(s)</a></span>
+                                                </div>
+                                                <div class="actions">
+                                                    <a href="http://ld-magento.template-help.com/magento_58878/checkout/cart/add/uenc/aHR0cDovL2xkLW1hZ2VudG8udGVtcGxhdGUtaGVscC5jb20vbWFnZW50b181ODg3OC8,/product/83/form_key/Pu88seeoujm3gNCs/" class="btns_cart" title="Add to Cart"><span class="fl-line-icon-set-shopping63"></span></a>
+                                                    <ul class="add-to-links">
+                                                        <li><a href="http://ld-magento.template-help.com/magento_58878/wishlist/index/add/product/83/form_key/Pu88seeoujm3gNCs/" class="link-wishlist"><span>Add to Wishlist</span></a></li>
+                                                        <li><a href="http://ld-magento.template-help.com/magento_58878/catalog/product_compare/add/product/83/uenc/aHR0cDovL2xkLW1hZ2VudG8udGVtcGxhdGUtaGVscC5jb20vbWFnZW50b181ODg3OC8,/form_key/Pu88seeoujm3gNCs/" class="link-compare"><span>Add to Compare</span></a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>       
+                                </div>
+
+                                <div class="carousel_col">
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/F58287" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/667562" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="carousel_col">
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/CDC4DE" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/FBAC37" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>    
+
+                                <div class="carousel_col">
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/71D7BC" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/F89974" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="carousel_col">
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/01504A" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/877D39" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="carousel_col">
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/C53B9B" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="item index_item">
+                                        <div class="item_placeholder"></div>
+                                        <div class="item_wrap">
+                                            <div class="product-image-container">
+                                                <a href="#" title="Suit blazer" class="product-image" itemprop="url">
+                                                    <img id="product-collection-image-83" src="https://dummyimage.com/360x446/62519F" alt="Suit blazer" width="360" height="446" itemprop="image">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- // END main-container -->
